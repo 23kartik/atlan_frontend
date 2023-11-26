d85357a542f37bbb7ca1d7370ee7b8ff8785c0d6
23kartik <kartik23tiwari@gmai.com>
2023-11-26 14:41:08 +0530

    removed mode

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import './tailwind.css';
 import  QueryContextProvider  from './components/QueryContextProvider';
+import reportWebVitals from './reportWebVitals';
 
 const App = lazy(() => import('./App'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <QueryContextProvider>
       <Suspense fallback={<div>Loading...</div>}>
         <App />
       </Suspense>
     </QueryContextProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
+reportWebVitals(console.log);
